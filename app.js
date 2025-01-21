20f245480ce65959400c54dbf5e31d095f737621
Daniel Fuentes <dnl243@hotmail.com.ar>
2025-01-21 11:25:19 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,13 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+"use strict";
+
+let amigos = [];
+
+function agregarAmigo(){
+
+}
+
+function SortearAmigo(){
+  
+}
